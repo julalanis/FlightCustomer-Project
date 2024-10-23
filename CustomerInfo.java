e5ecfd83766bcbcbda062920174ab5bb4d001115
Julie Alanis <136264672+julalanis@users.noreply.github.com>
2024-10-23 17:41:42 -0500

    Update CustomerInfo.java
    Updated with test print statements.

--- CustomerInfo.java
+++ CustomerInfo.java
@@ -1,5 +1,25 @@
 public class CustomerInfo {
     public static void main(String[] args) {
+        Customer customer1 = new Customer();
+        Customer customer2 = new Customer("Julie", 30);
+
+        FlightCustomer flightCustom1 = new FlightCustomer();
+        FlightCustomer flightCustom2 = new FlightCustomer("Tato", 7, 175, 590.99);
+
+        System.out.println(customer1.toString());
+
+        System.out.println();
+
+        System.out.println(customer2.toString());
+
+        System.out.println();
+
+        System.out.println(flightCustom1.toString());
+
+        System.out.println();
+
+        System.out.println(flightCustom2.toString());
+
 
     }
 }
