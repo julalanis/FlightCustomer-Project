163b36d21065f3c04dec0154165f67255218b541
Julie Alanis <136264672+julalanis@users.noreply.github.com>
2024-10-23 21:36:17 -0500

    Update RetailCustomer.java
    Added get/set methods, constructors, importance level based on average and toString method.

--- RetailCustomer.java
+++ RetailCustomer.java
@@ -1,2 +1,59 @@
-public class RetailCustomer {
+public class RetailCustomer extends Customer {
+    //named variables
+    private double totalSpent;
+    private int numberOfItemsPurchased;
+
+    //constructor with no parameters
+    RetailCustomer(){
+    }
+
+    //constructor with parameters
+    RetailCustomer(String name, int age, double totalSpent, int numberOfItemsPurchased){
+        setName(name);
+        setAge(age);
+        this.totalSpent = totalSpent;
+        this.numberOfItemsPurchased = numberOfItemsPurchased;
+    }
+
+    //get methods for 2 variables
+    double getTotalSpent(){
+        return totalSpent;
+    }
+
+    int getNumberOfItemsPurchased(){
+        return numberOfItemsPurchased;
+    }
+
+    //set methods for 2 variables
+    public void setTotalSpent(double totalSpent){
+        this.totalSpent = totalSpent;
+    }
+
+    public void setNumberOfItemsPurchased(int numberOfItemsPurchased){
+        this.numberOfItemsPurchased = numberOfItemsPurchased;
+    }
+
+    //importanceLevel method to override Customer
+    //values were not given so if average = $200+ Gold, $150-199 Silver, $100-149 Bronze, $0-99 Regular
+    @Override
+    public String importanceLevel() {
+        double average = totalSpent / numberOfItemsPurchased;
+        if (average >= 200) {
+            return "Gold";
+        } else if (average >= 150) {
+            return "Silver";
+        } else if (average >= 100) {
+            return "Bronze";
+        } else return "Regular";
+    }
+
+        //toString method
+        public String toString(){
+        return "Customer name: " + getName() + "\n" +
+                "Customer age: " + getAge() + "\n" +
+                "Customer's importance level: " + importanceLevel() + "\n" +
+                "Number of Items Purchased: " + getNumberOfItemsPurchased() + "\n" +
+                "Total Spent: " + getTotalSpent();
+        }
+
 }
