59987e3402b225b277b92c13c939932442ee1582
Julie Alanis <136264672+julalanis@users.noreply.github.com>
2024-10-23 22:08:59 -0500

    Update CustomerInfo.java
    Redone main, Customers now stored in an ArrayList. Loop to print each item.

--- CustomerInfo.java
+++ CustomerInfo.java
@@ -1,28 +1,34 @@
+import java.lang.Math;
+import java.util.ArrayList;
+
 public class CustomerInfo {
     public static void main(String[] args) {
+        //declare an Array of Customers: 2 Customers, 2 Flight Customers, 2 Retail Customers
+        ArrayList<Customer> customerList = new ArrayList<>();
         Customer customer1 = new Customer();
         Customer customer2 = new Customer("Julie", 30);
+        FlightCustomer flightCustomer1 = new FlightCustomer();
+        FlightCustomer flightCustomer2 = new FlightCustomer("Tato", 7, ((int)(Math.random() * 200)), 1002.56);
+        RetailCustomer retailCustomer1 = new RetailCustomer();
+        RetailCustomer retailCustomer2 = new RetailCustomer("Riley", 6, 454.65, 2);
 
-        FlightCustomer flightCustom1 = new FlightCustomer();
-        FlightCustomer flightCustom2 = new FlightCustomer("Tato", 7, 175, 590.99);
+        customerList.add(customer1);
+        customerList.add(customer2);
+        customerList.add(flightCustomer1);
+        customerList.add(flightCustomer2);
+        customerList.add(retailCustomer1);
+        customerList.add(retailCustomer2);
 
-        System.out.println(customer1.toString());
+        //loop to print out each Customer object
+        for (int i = 0; i < customerList.size(); i++){
+            System.out.println(customerList.get(i));
+            System.out.println();
+        }
 
-        System.out.println();
-
-        System.out.println(customer2.toString());
-
-        System.out.println();
-
-        System.out.println(flightCustom1.toString());
-
-        System.out.println();
-
-        System.out.println(flightCustom2.toString());
-
+/*
         System.out.println();
         customer1.setName("Customer1");
-        System.out.println(customer1.getName());
+        System.out.println(customer1.getName()); */
 
 
     }
